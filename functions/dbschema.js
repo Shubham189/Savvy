38173c6944e4a244f6ab7da37a2d8866658c094a
Shubham Patra <spatra.prof@gmail.com>
2020-10-05 01:28:19 +0530

    Add files via upload

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let bd={
+    questions: [
+        {
+            userHandle:  'user',
+            body: 'this is question body',
+            questionedAt: '2020-10-04T19:35:23.548Z',
+            upVoteCount: 5,
+            commentCount: 2
+        }
+    ]
+}
